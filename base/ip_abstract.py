9ca7d788348ddf1ac0345b987390de48fe3995ec
Felicià Maviane Macia <felicia.maviane-macia@inra.fr>
2020-07-17 13:21:03 +0200

    Sem-ver: bugfix - Added logging to dv_wrapper - app.py sends uncatched exceptions to log - Fixed exception when adding files - Fixed parameter when calling QFileDialog - Fixed not always updating output when executing partial pipelines - Simplified param widgets

--- base/ip_abstract.py
+++ base/ip_abstract.py
@@ -184,7 +184,6 @@ class AbstractImageProcessor(ImageWrapper):
                 new_error_kind="source_issue",
                 new_error_level=logging.ERROR,
                 target_logger=logger,
-                target_logger=logger,
             )
 
         return src_img
