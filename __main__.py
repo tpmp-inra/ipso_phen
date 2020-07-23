9e2a30ce6da18a12124842e1a587136c31e0bfd7
Felicià Maviane Macia <felicia.maviane-macia@inra.fr>
2020-07-23 11:55:19 +0200

    Sem-Ver: Bugfix Moved log initialisation to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,31 @@
 import sys
 import logging
+import os
+import datetime
+
+
+from ui_qt.qt_log_streamer import QtHandler
+
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(name)s - %(levelname)s] - %(message)s",
+    handlers=[
+        QtHandler(),
+        logging.FileHandler(
+            os.path.join("logs", f"{datetime.datetime.now().strftime('%Y%b%d %H%M%S')}.log"),
+            mode="a",
+            delay=True,
+        ),
+    ],
+)
+logger = logging.getLogger(__name__)
+logger.info("Starting IPSO Phen")
 
 from ipapi import __init__
 
-logger = logging.getLogger(__name__)
-logger.info("Starting IPSO Phen")
 
 from PySide2.QtWidgets import QApplication
 
