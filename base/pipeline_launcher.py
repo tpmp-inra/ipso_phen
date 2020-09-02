930ce0bef3273c77a2de6795e658477e0a4b484d
Felicià Maviane Macia <felicia.maviane-macia@inra.fr>
2020-09-02 16:01:28 +0200

    Sem-ver: bugfix - Fixed key error when retrieving callbacks

--- base/pipeline_launcher.py
+++ base/pipeline_launcher.py
@@ -195,8 +195,8 @@ def launch(**kwargs):
         group_by_series=res["generate_series_id"],
         store_images=False,
     )
-    pp.progress_callback = kwargs["progress_callback"]
-    pp.error_callback = kwargs["error_callback"]
+    pp.progress_callback = kwargs.get("progress_callback", None)
+    pp.error_callback = kwargs.get("error_callback", None)
     pp.ensure_root_output_folder()
     pp.accepted_files = image_list_
     pp.script = script
