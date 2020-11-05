9339d13496b7e1638bc025efb7180ef3b4917d80
Felicià Maviane Macia <felicia.maviane-macia@inra.fr>
2020-11-05 13:57:18 +0100

    Sem-ver: bugfix - Removed version from requirements - Made remove_version_numbers.py executable

--- scripts/remove_version_numbers.py
+++ scripts/remove_version_numbers.py
@@ -1,10 +1,16 @@
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), "..", "requirements.txt")
 
-with open(file=file_path, mode="r") as f:
-    lines = [line.split("==")[0] for line in f.read().split("\n")]
+def main():
+    file_path = os.path.join(os.path.dirname(__file__), "..", "requirements.txt")
 
-with open(file=file_path, mode="w") as f:
-    for line in lines:
-        f.write(f"{line}\n")
+    with open(file=file_path, mode="r") as f:
+        lines = [line.split("==")[0] for line in f.read().split("\n")]
+
+    with open(file=file_path, mode="w") as f:
+        for line in lines:
+            f.write(f"{line}\n")
+
+
+if __name__ == "__main__":
+    main()
