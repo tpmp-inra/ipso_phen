1c3c3263b1ad9db3dcd7d7afc8612a6bb94a6854
Felicià Maviane Macia <felicia.maviane-macia@inra.fr>
2022-06-22 14:42:20 +0200

    Updated requierements

--- scripts/remove_version_numbers.py
+++ scripts/remove_version_numbers.py
@@ -4,10 +4,10 @@ import os
 def main():
     file_path = os.path.join(os.path.dirname(__file__), "..", "requirements.txt")
 
-    with open(file=file_path, mode="r") as f:
+    with open(file=file_path, mode="r", encoding="utf-16") as f:
         lines = [line.split("==")[0] for line in f.read().split("\n")]
 
-    with open(file=file_path, mode="w") as f:
+    with open(file=file_path, mode="w", encoding="utf-16") as f:
         for line in lines:
             f.write(f"{line}\n")
 
